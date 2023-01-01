d0654b377cdcaef913cd3a96b45fd02c06ae4f13
nhunguyendinh1902 <nhunguyendinh75@gmail.com>
2023-01-01 22:19:26 +0700

    update

--- src/components/page/layout/menu.js
+++ src/components/page/layout/menu.js
@@ -2,6 +2,58 @@ import "../../css/menu.css";
 import "../../../App.css"
 
 function Menu() {
+    const listMenu = [
+        {
+            id: 1,
+            img: require("../../Assets/img/menu-item1.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 2,
+            img: require("../../Assets/img/menu-item2.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 3,
+            img: require("../../Assets/img/menu-item3.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 4,
+            img: require("../../Assets/img/menu-item.jpg"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 5,
+            img: require("../../Assets/img/menu-item5.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 6,
+            img: require("../../Assets/img/menu-item6.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 7,
+            img: require("../../Assets/img/menu-item6.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 8,
+            img: require("../../Assets/img/menu-item6.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 9,
+            img: require("../../Assets/img/menu-item6.png"),
+            title: "Lasagne al Forno"
+        },
+        {
+            id: 10,
+            img: require("../../Assets/img/menu-item6.png"),
+            title: "Lasagne al Forno"
+        }
+    ];
     return (
         <div>
             <section>
@@ -20,66 +72,80 @@ function Menu() {
                     <div className="container">
                         <div className="menu-box">
                             <div className="menu-list row">
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                {
+                                    listMenu.map(item => (
+                                        <div className="col-md-6 col-lg-4 col-sm-12" key={item.id}>
+                                            <div className="menu-item">
+                                                <a href="#" className="menu-img">
+                                                    <img src={item.img} alt="" />
+                                                </a>
+                                                <div className="menu-title">
+                                                    <h3><a href="#">{item.title}</a></h3>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                                {/* <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item.jpg")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item1.png")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item2.png")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item3.png")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item5.png")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 col-sm-12">
-                                    <div class="menu-item">
-                                        <a href="#" class="menu-img">
+                                <div className="col-md-6 col-lg-4 col-sm-12">
+                                    <div className="menu-item">
+                                        <a href="#" className="menu-img">
                                             <img src={require("../../Assets/img/menu-item6.png")} alt="" />
                                         </a>
-                                        <div class="menu-title">
+                                        <div className="menu-title">
                                             <h3><a href="#">Lasagne al Forno</a></h3>
                                         </div>
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
                             <div className="pagination">
                                 <ul id="pagination-menu">
